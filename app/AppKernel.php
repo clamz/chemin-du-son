e96c52885cb833600768456a1b47e368a3fa1e31
clamz <deathangel_99@hotmail.fr>
2011-04-16 13:38:46 +0200

    add the bundle  UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Acme\DemoBundle\AcmeDemoBundle(),
             new Cds\SiteBundle\CdsSiteBundle(),
+            new \Cds\UserBundle\CdsUserBundle,
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
